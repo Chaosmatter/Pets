42493c6a4196095c02bc2438273d16a06c37c6ab
Quinton <Chaosmatter@gmx.de>
2021-05-05 19:00:58 +0200

    added Config

--- src/main/java/de/chaosmatter/pets/Pets.java
+++ src/main/java/de/chaosmatter/pets/Pets.java
@@ -2,6 +2,7 @@ package de.chaosmatter.pets;
 
 import de.chaosmatter.pets.commands.PetCommand;
 import de.chaosmatter.pets.commands.PetManager;
+import de.chaosmatter.pets.config.ConfigManager;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,11 +11,14 @@ public class Pets extends JavaPlugin {
     @Getter
     private static Pets instance;
     private PetManager petManager;
+    private ConfigManager configManager;
 
     @Override
     public void onEnable() {
         instance = this;
         this.petManager = new PetManager(this);
+        this.configManager = new ConfigManager(this);
+
         getCommand("pet").setExecutor(new PetCommand(this));
         getCommand("pets").setExecutor(new PetCommand(this));
     }
---
+++ src/main/java/de/chaosmatter/pets/config/ConfigManager.java
@@ -0,0 +1,51 @@
+package de.chaosmatter.pets.config;
+
+import de.chaosmatter.pets.Pets;
+import lombok.Getter;
+
+import java.util.Objects;
+
+public class ConfigManager {
+    private final Pets plugin;
+
+    public ConfigManager(Pets plugin) {
+        this.plugin = plugin;
+        createConfig();
+    }
+
+    public void createConfig() {
+        this.plugin.getConfig().addDefault("Prefix", "&a&lPets §8»");
+        this.plugin.getConfig().addDefault("NoPermission", "%prefix% &cDafür hast du keine Berechtigung!");
+        this.plugin.getConfig().addDefault("CreatedPet", "%prefix% &eDein Haustier wurde erstellt!");
+        this.plugin.getConfig().addDefault("RemovedPet", "%prefix% &cDein Haustier wurde entfernt!");
+        this.plugin.getConfig().addDefault("RemovedAllPetsPlayer", "%prefix% &aDu hast erfolgreich alle Haustiere entfernt!");
+        this.plugin.getConfig().addDefault("RemovedAllPetsBroadcast", "%prefix% &4Es wurden alle Haustiere entfernt!");
+        this.plugin.getConfig().addDefault("UpdatedPetProperties", "%prefix% &eDein Haustier wurde aktualisiert.");
+
+        this.plugin.getConfig().options().copyDefaults(true);
+        this.plugin.saveConfig();
+
+        this.prefix = Objects.requireNonNull(this.plugin.getConfig().getString("Prefix")).replace("&", "§");
+        this.noPermission = Objects.requireNonNull(this.plugin.getConfig().getString("NoPermission")).replace("&", "§").replace("%prefix%", this.prefix);
+        this.createdPet = Objects.requireNonNull(this.plugin.getConfig().getString("CreatedPet")).replace("&", "§").replace("%prefix%", this.prefix);
+        this.removedPet = Objects.requireNonNull(this.plugin.getConfig().getString("RemovedPet")).replace("&", "§").replace("%prefix", this.prefix);
+        this.removedAllPetsPlayer = Objects.requireNonNull(this.plugin.getConfig().getString("RemovedAllPetsPlayer").replace("&", "§").replace("%prefix%", this.prefix));
+        this.removedAllPetsBroadcast = Objects.requireNonNull(this.plugin.getConfig().getString("RemovedAllPetsBroadcast").replace("&", "§").replace("%prefix%", this.prefix));
+        this.updatedPet = Objects.requireNonNull(this.plugin.getConfig().getString("UpdatedPetProperties").replace("&", "§").replace("%prefix%", this.prefix));
+    }
+
+    @Getter
+    private String prefix;
+    @Getter
+    private String noPermission;
+    @Getter
+    private String createdPet;
+    @Getter
+    private String removedPet;
+    @Getter
+    private String removedAllPetsPlayer;
+    @Getter
+    private String removedAllPetsBroadcast;
+    @Getter
+    private String updatedPet;
+}
